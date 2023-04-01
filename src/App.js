06d0304722e06db3c2974510e953280d7bd1c11d
Torben23 <105077862+Torben23@users.noreply.github.com>
2023-04-01 05:46:42 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import torben from './imagenes/torben.png';
+
 import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
     <div className='aplicacion-tareas'>
-      <div className='torben-logo-contenedor'>
-        <img 
-          src= {torben} 
-          className='torben-logo' />
-      </div>   
+      
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
         <ListaDeTareas />
